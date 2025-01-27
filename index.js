f2ad8c95e4dc03eb6b3f26326fdbbc5663fbb137
yapranav09v <yapranav3@gmail.com>
2025-01-27 11:01:08 +0530

    initial deployment

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { default: firebase } = require('firebase/compat/app');
+const path = require('path')
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+//creating a dynamic rout
+app.get("/profile/:username", function(req, res){
+    res.send(`Name is, ${req.params.username}`);
+})
+
+app.listen(3000, ()=>{console.log("Running at localhost:3000")});
